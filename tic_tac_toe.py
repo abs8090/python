fb95ea03e0fdb72094182e1ad5d41ccf588f1cf3
abs8090 <km.5432@hotmail.com>
2019-03-29 04:46:42 +0300

    drawing board based on user input

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,1 +1,60 @@
 print("this is a tic tac toe python project")
+boardList = []
+
+r = 0
+c = 0
+numberOfRows = 0
+numberOfCols = 0
+
+withPlus = "-+"
+withOutPlus = "-"
+
+numberOfRows = int(input("enter how many rows:"))
+numberOfCols = int(input("enter how many columns:"))
+
+def initiateBoard():
+    for row in range(numberOfRows):
+        rowList = []
+        for col in range(numberOfCols):
+            rowList.append("A")
+        boardList.append(rowList)
+
+def drawBoard():
+    rowCounter = 0
+    for row in boardList:
+        colCounter = 0
+        for box in row:
+            # draw boxes and | for each row
+            if colCounter < numberOfRows - 1:
+                print("{}|".format(box), end='')
+            else:
+                print("{}".format(box), end='')
+            colCounter = colCounter + 1
+        print()
+        # end of draw boxes and | for each row
+        colCounter = 0
+        if rowCounter < numberOfRows - 1:
+            for lineSeparator in row:
+                if colCounter < numberOfRows - 1:
+                    print("{}".format(withPlus), end='')
+                else:
+                    print("{}".format(withOutPlus), end='')
+                colCounter = colCounter + 1
+            print()
+        rowCounter = rowCounter + 1
+
+
+
+initiateBoard()
+drawBoard()
+#while True:
+#    drawBoard()
+#    print("player 1")
+#    r = int(input("enter which row:"))
+#    c = int(input("enter which box:"))
+#    boardList[r][c] = "O"
+#    drawBoard()
+#    print("player 2")
+#    r = int(input("enter which row:"))
+#    c = int(input("enter which box:"))
+#    boardList[r][c] = "X"
